bd9ee4a6cca76f07ddcd43d863f570c80fd03a2f
Wybe <wybe@ruurdwestra.nl>
2018-02-11 19:54:59 +0100

    + Added basic observer code.

--- auto_factuur/main.py
+++ auto_factuur/main.py
@@ -1,4 +1,8 @@
 import os
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
 
 TEST_DIRECTORY = "../test_dir/"
@@ -9,6 +13,26 @@ def main():
         os.makedirs(TEST_DIRECTORY)
 
     os.chdir(TEST_DIRECTORY)
+
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s] - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, TEST_DIRECTORY, recursive=False)
+    observer.start()
+
+    logging.info("Setup complete.")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logging.info("Exiting...")
+        observer.stop()
+
+    observer.join()
 
 
 if __name__ == "__main__":
