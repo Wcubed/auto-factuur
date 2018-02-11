4519427589636d01c9101f2bba78e5e4cf9b5748
Wybe <wybe@ruurdwestra.nl>
2018-02-11 21:14:48 +0100

    + Can now open mail clients.

--- auto_factuur/mail.py
+++ auto_factuur/mail.py
@@ -1,3 +1,27 @@
+import os
+import sys
+import logging
+import subprocess
 
-def construct_mail():
-    return
+def open_mail_program():
+    title = "Test title."
+    body = "Testing body."
+
+    url = "mailto:?subject={}&body={}"
+    url = url.format(title, body)
+
+
+    platform = sys.platform
+
+    # TODO: Test this vvv.
+    # TODO: Make this return nice errors when stuff fails vvv.
+
+    if platform == 'win32':
+        os.startfile(url)
+    elif platform == 'darwin':
+        subprocess.Popen(['open', url])
+    elif platform == 'linux':
+        subprocess.Popen(['xdg-email', url])
+    else:
+        logging.error("Can't open mail client. Unknown OS: " + platform)
+        return
--- auto_factuur/pdf_event_handler.py
+++ auto_factuur/pdf_event_handler.py
@@ -1,6 +1,8 @@
 import logging
 import os
+
 from watchdog.events import FileSystemEventHandler
+import auto_factuur.mail as mail
 
 
 class PdfEventHandler(FileSystemEventHandler):
@@ -22,6 +24,9 @@ class PdfEventHandler(FileSystemEventHandler):
         if is_file_pdf(potential_pdf_path):
             # The filename looks like this might be a pdf.
             logging.info("file is a pdf")
+
+            mail.construct_mail()
+
 
 
 def is_file_pdf(src_path):
